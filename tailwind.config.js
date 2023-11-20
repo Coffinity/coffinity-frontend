b0832c024f8102abc83ca5498d745cae4c5404ff
Thadchanon <thadchanonjirarat@gmail.com>
2023-11-20 17:44:31 +0700

    feat:footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        footerTexture: "url('src/assets/footerBackground.png')",
+      },
+    },
   },
   plugins: [require('daisyui')],
 }
