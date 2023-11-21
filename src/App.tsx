26139886bd6818992192d186908ddc9b9eee94aa
Thadchanon <thadchanonjirarat@gmail.com>
2023-11-21 15:45:53 +0700

    feat: equipment component

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import Equipment from './components/Equipment'
 import Footer from './components/Footer'
 import Header from './components/Header'
 import Navbar from './components/Navbar'
@@ -8,6 +9,7 @@ function App() {
     <>
       <Navbar />
       <Header />
+      <Equipment />
       <Home />
       <Footer />
     </>
