eda6ebc2fdce563ac30dead8dcfa0a6e1becf542
imnotprn <pepand.napoleon@hotmail.com>
2023-11-22 13:05:11 -0800

    fix/header2/detail

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,7 @@
 export default function Header() {
   return (
     <div>
-      <div className="min-h-screen flex justify-center items-center bg-header bg-fixed">
+      <div className="min-h-screen flex justify-end items-center bg-header bg-fixed">
         <p className="w-[531px] h-[91px] text-neutral-400 text-2xl font-normal ml-[1400px] mt-[800px]">
           OPEN 7.30AM - 5.00PM
           <br />
