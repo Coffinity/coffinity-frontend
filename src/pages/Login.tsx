a0edce863e92359003ea3f2845b97ac81498154f
peetwerapat <werapatsaejong@gmail.com>
2023-11-23 18:47:33 +0700

    add feat/authProvider

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,36 +1,60 @@
+import { useNavigate } from 'react-router-dom'
+import { useAuth } from '../providers/AuthProvider'
+import { FormEvent, useState } from 'react'
+
 export default function Login() {
+  const { login } = useAuth()
+  const navigate = useNavigate()
+  const [username, setUsername] = useState<string>('')
+  const [password, setPassword] = useState<string>('')
+
+  const handleSubmit = async (e: FormEvent) => {
+    e.preventDefault()
+
+    try {
+      await login({ username, password })
+
+      navigate('/')
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
   return (
     <div className="h-screen flex justify-center items-center ">
       <form className="relative space-y-3 rounded-md bg-white p-6 shadow-xl lg:p-10 border border-gray-100">
         <h1 className="text-xl font-semibold lg:text-2xl flex items-center">SIGN IN</h1>
         <p className="pb-4 text-gray-500">Sign in to access your account</p>
 
-        <div className="">
-          <label className=""> Email Address </label>
+        <form onSubmit={handleSubmit}>
+          <label className=""> Username </label>
           <input
-            type="email"
+            type="text"
+            onChange={(e) => setUsername(e.target.value)}
             placeholder="Info@example.com"
             className="mt-2 h-12 w-full rounded-md bg-gray-100 px-3 outline-none focus:ring"
           />
-        </div>
-        <div>
-          <label className=""> Password </label>
-          <input
-            type="password"
-            placeholder="******"
-            className="mt-2 h-12 w-full rounded-md bg-gray-100 px-3 outline-none focus:ring"
-          />
-        </div>
 
-        <div>
-          <p className="pb-1 text-gray-500 flex justify-center">Forgot Password?</p>
-          <button
-            type="button"
-            className="mt-5 w-full rounded-md bg-black p-2 text-center font-semibold text-white outline-none focus:ring"
-          >
-            Get Started
-          </button>
-        </div>
+          <div>
+            <label className=""> Password </label>
+            <input
+              type="password"
+              onChange={(e) => setPassword(e.target.value)}
+              placeholder="******"
+              className="mt-2 h-12 w-full rounded-md bg-gray-100 px-3 outline-none focus:ring"
+            />
+          </div>
+
+          <div>
+            <p className="pb-1 text-gray-500 flex justify-center">Forgot Password?</p>
+            <button
+              type="submit"
+              className="mt-5 w-full rounded-md bg-black p-2 text-center font-semibold text-white outline-none focus:ring"
+            >
+              Get Started
+            </button>
+          </div>
+        </form>
       </form>
     </div>
   )
---
+++ src/providers/AuthProvider.tsx
@@ -0,0 +1,75 @@
+import { ReactNode, createContext, useContext, useState } from 'react'
+import { IAccessToken, ICreateUserDTO, ILogin, IUserDTO } from '../types/dto'
+import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
+
+interface IAuthProviderProps {
+  children: ReactNode
+}
+
+interface IAuthContextType {
+  isLoggedIn: boolean
+  username: string | null
+  login: (loginBody: ILogin) => Promise<void>
+  logout: () => void
+  register: (body: ICreateUserDTO) => Promise<void>
+}
+
+const AuthContext = createContext<IAuthContextType | null>(null)
+
+export const useAuth = () => {
+  const context = useContext(AuthContext)
+
+  if (!context) throw new Error('useAuth must be used within AuthProvider')
+
+  return context
+}
+
+const user = localStorage.getItem('username')
+
+const AuthProvider = ({ children }: IAuthProviderProps) => {
+  const navigate = useNavigate()
+  const [isLoggedIn, setIsLoggedin] = useState<boolean>(false)
+  const [username, setUsername] = useState<string | null>(user)
+
+  const register = async (registerBody: ICreateUserDTO) => {
+    try {
+      await axios.post<IUserDTO>('http://localhost:8080/users', registerBody, {
+        headers: { 'Content-Type': 'application/json' },
+      })
+      navigate('/login')
+    } catch (error) {
+      throw new Error('Can not Sign up !')
+    }
+  }
+
+  const login = async (loginBody: ILogin) => {
+    const { username } = loginBody
+
+    try {
+      const res = await axios.post<IAccessToken>('http://localhost:8080/auth/login', loginBody, {
+        headers: { 'Content-Type': 'application/json' },
+      })
+
+      localStorage.setItem('token', res.data.accessToken)
+      localStorage.setItem('username', username)
+      setIsLoggedin(true)
+      setUsername(username)
+    } catch (err) {
+      throw new Error('Invalid username or password')
+    }
+  }
+  const logout = () => {
+    localStorage.clear()
+    setIsLoggedin(false)
+    setUsername(null)
+
+    navigate('/')
+  }
+
+  return (
+    <AuthContext.Provider value={{ register, isLoggedIn, username, login, logout }}>{children}</AuthContext.Provider>
+  )
+}
+
+export default AuthProvider
