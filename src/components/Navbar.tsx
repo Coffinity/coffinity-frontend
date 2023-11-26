9d229206d17a8e96a8d8ce2511235526fb690287
Thadchanon <thadchanonjirarat@gmail.com>
2023-11-26 14:23:57 +0700

    feat: add navber navigatiion path

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -12,9 +12,13 @@ const Navbar: FC<Props> = ({ onCoffeeBeanCick, onEquipmentClick, onFooterClick }
     <div>
       <div className="navbar w-full z-50 fixed bg-black text-neutral-content bg-opacity-50 justify-between">
         <div className="flex">
-          <h1 className="text-white m-5 font-kanit text-4xl">ดัมมะชาติ</h1>
+          <a href="/">
+            <h1 className="text-white m-5 font-kanit text-4xl">ดัมมะชาติ</h1>
+          </a>
           <div className="flex py-10 mt-13">
-            <button className="hover:text-white px-5">HOME</button>
+            <a href="/">
+              <button className="hover:text-white px-5">HOME</button>
+            </a>
             <button className="hover:text-white px-5" onClick={onCoffeeBeanCick}>
               COFFEE
             </button>
@@ -31,12 +35,18 @@ const Navbar: FC<Props> = ({ onCoffeeBeanCick, onEquipmentClick, onFooterClick }
           <div className="flex gap-10 items-start  ">
             <input type="text" placeholder="Search" className="input input-bordered w-24 md:w-auto" />
             {/* <SearchIcon fontSize="large" className="text-white large " /> */}
-            <LocalMallIcon sx={{ fontSize: 40 }} className="text-white text-5xl item-center" />
+            <a href="/cart">
+              <LocalMallIcon sx={{ fontSize: 40 }} className="text-white text-5xl item-center" />
+            </a>
           </div>
           <div className="flex">
-            <p className="text-white mx-10">REGISTER</p>
+            <a href="/register">
+              <p className="text-white mx-10">REGISTER</p>
+            </a>
             <p className="text-white">/</p>
-            <p className="text-white mx-10">SIGN IN</p>
+            <a href="/login">
+              <p className="text-white mx-10">SIGN IN</p>
+            </a>
           </div>
         </div>
       </div>
