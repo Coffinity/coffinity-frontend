b0b47e96e0908b6d591350bb37bf09cbec6d2b0b
peetwerapat <werapatsaejong@gmail.com>
2023-11-30 20:19:38 +0700

    feat/isLoading

--- src/pages/Checkout.tsx
+++ src/pages/Checkout.tsx
@@ -24,6 +24,7 @@ export const Checkout = () => {
   const { createCheckout } = useCheckout()
   const { cartState } = useContext(CartContext)
   const { cartList, totalPrice } = cartState
+  const { isLoading } = useCheckout()
 
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     setState((prev) => ({
@@ -70,148 +71,158 @@ export const Checkout = () => {
   }
 
   return (
-    <div className="min-h-screen pt-24 bg-allPageBg bg-cover bg-center bg-no-repeat flex flex-col items-center  ">
-      <div className="flex gap-10 max-md:flex-col-reverse ">
-        <div className="">
-          <div className="flex my-10">
-            <h1 className="text-3xl text-white font-bold">CHECK OUT</h1>
-          </div>
-          <div className="my-10 bg-white bg-opacity-80 px-8 pt-8 lg:mt-0 rounded-lg w-[670px] ">
-            <p className="text-xl font-semibold lg:text-2xl flex items-center text-black">SHIPPING ADRESS</p>
-
+    <div className="min-h-screen justify-center items-center bg-allPageBg bg-cover ">
+      {isLoading ? (
+        <span className="loading loading-spinner loading-lg"></span>
+      ) : (
+        <div className="pt-24 flex flex-col items-center">
+          <div className="flex gap-10 max-md:flex-col-reverse ">
             <div className="">
-              <div className="">
-                <div className="relative flex flex-col mt-5 gap-y-5">
-                  <input
-                    type="text"
-                    id="fullname"
-                    name="fullname"
-                    value={state.fullname}
-                    className="w-full rounded-md border border-gray-200 px-4 py-3 pl-11 text-sm shadow-sm outline-none focus:z-10 focus:border-blue-500 focus:ring-blue-500"
-                    placeholder="Full Name"
-                    onChange={handleChange}
-                    required
-                  />
-                  <input
-                    type="text"
-                    id="addressLine"
-                    name="addressLine"
-                    value={state.addressLine}
-                    className="w-full rounded-md border border-gray-200 px-4 py-3 pl-11 text-sm shadow-sm outline-none focus:z-10 focus:border-blue-500 focus:ring-blue-500"
-                    placeholder="Address_Line"
-                    onChange={handleChange}
-                    required
-                  />
-                  <input
-                    type="text"
-                    id="district"
-                    name="district"
-                    value={state.district}
-                    className="w-full rounded-md border border-gray-200 px-4 py-3 pl-11 text-sm shadow-sm outline-none focus:z-10 focus:border-blue-500 focus:ring-blue-500"
-                    placeholder="District"
-                    onChange={handleChange}
-                    required
-                  />
-                  <input
-                    type="text"
-                    id="province"
-                    name="province"
-                    value={state.province}
-                    className="w-full rounded-md border border-gray-200 px-4 py-3 pl-11 text-sm shadow-sm outline-none focus:z-10 focus:border-blue-500 focus:ring-blue-500"
-                    placeholder="Province"
-                    onChange={handleChange}
-                    required
-                  />
-                  <input
-                    type="text"
-                    id="postcode"
-                    name="postcode"
-                    value={state.postcode}
-                    className="w-full rounded-md border border-gray-200 px-4 py-3 pl-11 text-sm shadow-sm outline-none focus:z-10 focus:border-blue-500 focus:ring-blue-500"
-                    placeholder="Postcode"
-                    onChange={handleChange}
-                    required
-                  />
-                  <input
-                    type="text"
-                    id="phoneNumber"
-                    name="phoneNumber"
-                    value={state.phoneNumber}
-                    className="w-full rounded-md border border-gray-200 px-4 py-3 pl-11 text-sm shadow-sm outline-none focus:z-10 focus:border-blue-500 focus:ring-blue-500"
-                    placeholder="Phone_Number"
-                    onChange={handleChange}
-                    required
-                  />
-                </div>
+              <div className="flex my-10">
+                <h1 className="text-3xl text-white font-bold">CHECK OUT</h1>
               </div>
+              <div className="my-10 bg-white bg-opacity-80 px-8 pt-8 lg:mt-0 rounded-lg w-[670px] ">
+                <p className="text-xl font-semibold lg:text-2xl flex items-center text-black">SHIPPING ADRESS</p>
 
-              <div className="mt-6 border-t border-b py-2 border-black">
-                <div className="flex items-center justify-between">
-                  <p className="text-sm font-normal text-gray-500">Subtotal</p>
-                  <p className="font-semibold text-gray-900">{totalPrice} THB</p>
-                </div>
-                <div className="flex items-center justify-between">
-                  <p className="text-sm font-normal text-gray-500">Shipping</p>
-                  <p className="font-semibold text-gray-900">FREE</p>
-                </div>
-              </div>
-              <div className="mt-6 flex items-center justify-between">
-                <p className="text-sm font-medium text-gray-900">Total</p>
-
-                <p className="text-2xl font-semibold text-gray-900">
-                  {' '}
-                  <span className="text-xs font-normal text-gray-400">THB </span>
-                  {totalPrice}
-                </p>
-              </div>
-            </div>
-            <div className="mt-6 mb-8 text-center">
-              <button
-                type="button"
-                onClick={() => onPlaceHolderClick()}
-                className="mt-4 mb-8 group inline-flex w-full items-center justify-center rounded-md bg-black px-6 py-4 text-lg font-semibold text-white transition-all duration-200 ease-in-out focus:shadow hover:bg-gray-500"
-              >
-                Place Order
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="group-hover:ml-8 ml-4 h-6 w-6 transition-all"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                  strokeWidth="2"
-                >
-                  <path strokeLinecap="round" strokeLinejoin="round" d="M13 7l5 5m0 0l-5 5m5-5H6" />
-                </svg>
-              </button>
-            </div>
-            {/* <button className="mt-4 mb-8 w-full rounded-md bg-gray-900 px-6 py-3 font-medium text-white">
-              Place Order
-            </button> */}
-          </div>
-        </div>
-        <div>
-          <div className="">
-            <div className="flex my-10">
-              <p className="text-3xl text-white font-bold">ORDER SUMMARY</p>
-            </div>
-            <div className="bg-white bg-opacity-80 px-8 py-8 lg:mt-0 rounded-lg h-[386px] overflow-y-auto">
-              {cartList.map((item, index) => (
-                <div key={index} className="">
-                  <div className="flex rounded-lg  sm:flex-row">
-                    <img className="m-2 h-24 w-28 object-cover object-center" src={item.image} alt="product image" />
-                    <div className="flex w-full flex-col px-4 py-4">
-                      <span className="text-m font-semibold text-black">{item.name}</span>
-                      <span className="text-sm font-normal text-gray">{item.quantity}x qty</span>
-
-                      <p className="text-l font-semibold text-black">{item.price * item.quantity} THB</p>
+                <div className="">
+                  <div className="">
+                    <div className="relative flex flex-col mt-5 gap-y-5">
+                      <input
+                        type="text"
+                        id="fullname"
+                        name="fullname"
+                        value={state.fullname}
+                        className="w-full rounded-md border border-gray-200 px-4 py-3 pl-11 text-sm shadow-sm outline-none focus:z-10 focus:border-blue-500 focus:ring-blue-500"
+                        placeholder="Full Name"
+                        onChange={handleChange}
+                        required
+                      />
+                      <input
+                        type="text"
+                        id="addressLine"
+                        name="addressLine"
+                        value={state.addressLine}
+                        className="w-full rounded-md border border-gray-200 px-4 py-3 pl-11 text-sm shadow-sm outline-none focus:z-10 focus:border-blue-500 focus:ring-blue-500"
+                        placeholder="Address_Line"
+                        onChange={handleChange}
+                        required
+                      />
+                      <input
+                        type="text"
+                        id="district"
+                        name="district"
+                        value={state.district}
+                        className="w-full rounded-md border border-gray-200 px-4 py-3 pl-11 text-sm shadow-sm outline-none focus:z-10 focus:border-blue-500 focus:ring-blue-500"
+                        placeholder="District"
+                        onChange={handleChange}
+                        required
+                      />
+                      <input
+                        type="text"
+                        id="province"
+                        name="province"
+                        value={state.province}
+                        className="w-full rounded-md border border-gray-200 px-4 py-3 pl-11 text-sm shadow-sm outline-none focus:z-10 focus:border-blue-500 focus:ring-blue-500"
+                        placeholder="Province"
+                        onChange={handleChange}
+                        required
+                      />
+                      <input
+                        type="text"
+                        id="postcode"
+                        name="postcode"
+                        value={state.postcode}
+                        className="w-full rounded-md border border-gray-200 px-4 py-3 pl-11 text-sm shadow-sm outline-none focus:z-10 focus:border-blue-500 focus:ring-blue-500"
+                        placeholder="Postcode"
+                        onChange={handleChange}
+                        required
+                      />
+                      <input
+                        type="text"
+                        id="phoneNumber"
+                        name="phoneNumber"
+                        value={state.phoneNumber}
+                        className="w-full rounded-md border border-gray-200 px-4 py-3 pl-11 text-sm shadow-sm outline-none focus:z-10 focus:border-blue-500 focus:ring-blue-500"
+                        placeholder="Phone_Number"
+                        onChange={handleChange}
+                        required
+                      />
                     </div>
                   </div>
+
+                  <div className="mt-6 border-t border-b py-2 border-black">
+                    <div className="flex items-center justify-between">
+                      <p className="text-sm font-normal text-gray-500">Subtotal</p>
+                      <p className="font-semibold text-gray-900">{totalPrice} THB</p>
+                    </div>
+                    <div className="flex items-center justify-between">
+                      <p className="text-sm font-normal text-gray-500">Shipping</p>
+                      <p className="font-semibold text-gray-900">FREE</p>
+                    </div>
+                  </div>
+                  <div className="mt-6 flex items-center justify-between">
+                    <p className="text-sm font-medium text-gray-900">Total</p>
+
+                    <p className="text-2xl font-semibold text-gray-900">
+                      {' '}
+                      <span className="text-xs font-normal text-gray-400">THB </span>
+                      {totalPrice}
+                    </p>
+                  </div>
                 </div>
-              ))}
+                <div className="mt-6 mb-8 text-center">
+                  <button
+                    type="button"
+                    onClick={() => onPlaceHolderClick()}
+                    className="mt-4 mb-8 group inline-flex w-full items-center justify-center rounded-md bg-black px-6 py-4 text-lg font-semibold text-white transition-all duration-200 ease-in-out focus:shadow hover:bg-gray-500"
+                  >
+                    Place Order
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="group-hover:ml-8 ml-4 h-6 w-6 transition-all"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      strokeWidth="2"
+                    >
+                      <path strokeLinecap="round" strokeLinejoin="round" d="M13 7l5 5m0 0l-5 5m5-5H6" />
+                    </svg>
+                  </button>
+                </div>
+                {/* <button className="mt-4 mb-8 w-full rounded-md bg-gray-900 px-6 py-3 font-medium text-white">
+              Place Order
+            </button> */}
+              </div>
+            </div>
+            <div>
+              <div className="">
+                <div className="flex my-10">
+                  <p className="text-3xl text-white font-bold">ORDER SUMMARY</p>
+                </div>
+                <div className="bg-white bg-opacity-80 px-8 py-8 lg:mt-0 rounded-lg h-[386px] overflow-y-auto">
+                  {cartList.map((item, index) => (
+                    <div key={index} className="">
+                      <div className="flex rounded-lg  sm:flex-row">
+                        <img
+                          className="m-2 h-24 w-28 object-cover object-center"
+                          src={item.image}
+                          alt="product image"
+                        />
+                        <div className="flex w-full flex-col px-4 py-4">
+                          <span className="text-m font-semibold text-black">{item.name}</span>
+                          <span className="text-sm font-normal text-gray">{item.quantity}x qty</span>
+
+                          <p className="text-l font-semibold text-black">{item.price * item.quantity} THB</p>
+                        </div>
+                      </div>
+                    </div>
+                  ))}
+                </div>
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      )}
     </div>
   )
 }
--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,129 +1,133 @@
 import useProfile from '../hooks/userProfile'
 
 export const Profile = () => {
-  const { profile: userInfo } = useProfile()
+  const { profile: userInfo, isLoading } = useProfile()
 
   if (!userInfo) return null
 
   return (
     <div>
       <div className="min-h-screen w-full flex justify-center items-center bg-allPageBg bg-cover bg-fixed ">
-        <div className="relative w-1/2 bg-white shadow-xl p-10 rounded-md bg-opacity-80 mt-52 mb-28 text-black border border-gray-100">
-          <h1 className="text-xl font-semibold lg:text-2xl flex items-center justify-center">ACCOUNT DETAILS</h1>
-          <div className="flex gap-5 text-gray-500  justify-center  mt-3 pb-4">
-            <div className="flex ">
-              <h2> Username :&nbsp; </h2>
-              <p>{userInfo.username}</p>
+        {isLoading ? (
+          <span className="loading loading-spinner loading-lg"></span>
+        ) : (
+          <div className="relative w-1/2 bg-white shadow-xl p-10 rounded-md bg-opacity-80 mt-52 mb-28 text-black border border-gray-100">
+            <h1 className="text-xl font-semibold lg:text-2xl flex items-center justify-center">ACCOUNT DETAILS</h1>
+            <div className="flex gap-5 text-gray-700  justify-center  mt-3 pb-4">
+              <div className="flex ">
+                <h2> Username :&nbsp; </h2>
+                <p>{userInfo.username}</p>
+              </div>
+              <div className="flex">
+                <h2> Email :&nbsp; </h2>
+                <p>{userInfo.email}</p>
+              </div>
             </div>
-            <div className="flex">
-              <h2> Email :&nbsp; </h2>
-              <p>{userInfo.email}</p>
+
+            <div className="mt-3 flex justify-center">
+              <h1 className="text-xl font-semibold lg:text-xl flex items-center justify-center">ORDER HISTORY</h1>
+            </div>
+
+            <div className="">
+              {/* mx-auto mt-3 p overflow-hidden rounded-xl border shadow  */}
+              <table className="w-full border-separate border-spacing-y-2 border-spacing-x-2 mt-6 border-t border-b py-2 border-black">
+                <thead className="hidden w-full border-b lg:table-header-group">
+                  <tr className="w-full">
+                    <td width="50%" className="whitespace-normal py-4 text-sm font-bold text-gray-500 sm:px-6">
+                      Order
+                    </td>
+
+                    <td className="whitespace-normal py-4 text-sm font-bold text-gray-500 sm:px-6">Date</td>
+
+                    <td className="whitespace-normal py-4 text-sm font-bold text-gray-500 sm:px-6">Amount</td>
+
+                    <td className="whitespace-normal py-4 text-sm font-bold text-gray-500 sm:px-6">Status</td>
+                  </tr>
+                </thead>
+
+                <tbody className="lg:border-gray-300">
+                  <tr>
+                    <td className="whitespace-no-wrap py-4 text-sm font-bold text-gray-700 sm:px-6">
+                      #000001
+                      <div className="mt-1 lg:hidden ">
+                        <p className="font-normal text-gray-500">15 December, 2021</p>
+                      </div>
+                    </td>
+
+                    <td className="whitespace-no-wrap hidden py-4 text-sm font-normal text-gray-500 sm:px-6 lg:table-cell w-full">
+                      15 December, 2021
+                    </td>
+
+                    <td className="whitespace-no-wrap py-4 px-6 text-right text-sm text-gray-600 lg:text-left">
+                      500.00 THB
+                      <div className="flex mt-1 ml-auto w-fit items-center rounded-full bg-black py-2 px-3 text-left text-xs font-bold text-white lg:hidden">
+                        Complete
+                      </div>
+                    </td>
+
+                    <td className="whitespace-no-wrap hidden py-4 text-sm font-normal text-gray-500 sm:px-6 lg:table-cell">
+                      <div className="inline-flex items-center rounded-full bg-black py-2 px-3 text-xs text-white">
+                        Complete
+                      </div>
+                    </td>
+                  </tr>
+
+                  <tr>
+                    <td width="50%" className="whitespace-no-wrap py-4 text-sm font-bold text-gray-700 sm:px-6">
+                      #000002
+                      <div className="mt-1 lg:hidden">
+                        <p className="font-normal text-gray-500">14 November, 2021</p>
+                      </div>
+                    </td>
+
+                    <td className="whitespace-no-wrap hidden py-4 text-sm font-normal text-gray-500 sm:px-6 lg:table-cell">
+                      14 November, 2021
+                    </td>
+
+                    <td className="whitespace-no-wrap py-4 px-6 text-right text-sm text-gray-600 lg:text-left">
+                      500.00 THB
+                      <div className="flex mt-1 ml-auto w-fit items-center rounded-full bg-gray-500 py-1 px-2 text-left font-bold text-white lg:hidden">
+                        Pending
+                      </div>
+                    </td>
+
+                    <td className="whitespace-no-wrap hidden py-4 text-sm font-normal text-gray-500 sm:px-6 lg:table-cell">
+                      <div className="inline-flex items-center rounded-full bg-gray-500 py-1 px-2 text-white">
+                        Pending
+                      </div>
+                    </td>
+                  </tr>
+
+                  <tr>
+                    <td width="50%" className="whitespace-no-wrap py-4 text-sm font-bold text-gray-700 sm:px-6">
+                      #000003
+                      <div className="mt-1 lg:hidden">
+                        <p className="font-normal text-gray-500">15 October, 2021</p>
+                      </div>
+                    </td>
+
+                    <td className="whitespace-no-wrap hidden py-4 text-sm font-normal text-gray-500 sm:px-6 lg:table-cell">
+                      15 October, 2021
+                    </td>
+
+                    <td className="whitespace-no-wrap py-4 px-6 text-right text-sm text-gray-600 lg:text-left">
+                      500.00 THB
+                      <div className="flex mt-1 ml-auto w-fit items-center rounded-full bg-black py-2 px-3 text-left text-xs font-bold text-white lg:hidden">
+                        Complete
+                      </div>
+                    </td>
+
+                    <td className="whitespace-no-wrap hidden py-4 text-sm font-normal text-gray-500 sm:px-6 lg:table-cell">
+                      <div className="inline-flex items-center rounded-full bg-black py-2 px-3 text-xs text-white">
+                        Complete
+                      </div>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
             </div>
           </div>
-
-          <div className="mt-3 flex justify-center">
-            <h1 className="text-xl font-semibold lg:text-xl flex items-center justify-center">ORDER HISTORY</h1>
-          </div>
-
-          <div className="">
-            {/* mx-auto mt-3 p overflow-hidden rounded-xl border shadow  */}
-            <table className="w-full border-separate border-spacing-y-2 border-spacing-x-2 mt-6 border-t border-b py-2 border-black">
-              <thead className="hidden w-full border-b lg:table-header-group">
-                <tr className="w-full">
-                  <td width="50%" className="whitespace-normal py-4 text-sm font-bold text-gray-500 sm:px-6">
-                    Order
-                  </td>
-
-                  <td className="whitespace-normal py-4 text-sm font-bold text-gray-500 sm:px-6">Date</td>
-
-                  <td className="whitespace-normal py-4 text-sm font-bold text-gray-500 sm:px-6">Amount</td>
-
-                  <td className="whitespace-normal py-4 text-sm font-bold text-gray-500 sm:px-6">Status</td>
-                </tr>
-              </thead>
-
-              <tbody className="lg:border-gray-300">
-                <tr>
-                  <td className="whitespace-no-wrap py-4 text-sm font-bold text-gray-700 sm:px-6">
-                    #000001
-                    <div className="mt-1 lg:hidden ">
-                      <p className="font-normal text-gray-500">15 December, 2021</p>
-                    </div>
-                  </td>
-
-                  <td className="whitespace-no-wrap hidden py-4 text-sm font-normal text-gray-500 sm:px-6 lg:table-cell w-full">
-                    15 December, 2021
-                  </td>
-
-                  <td className="whitespace-no-wrap py-4 px-6 text-right text-sm text-gray-600 lg:text-left">
-                    500.00 THB
-                    <div className="flex mt-1 ml-auto w-fit items-center rounded-full bg-black py-2 px-3 text-left text-xs font-bold text-white lg:hidden">
-                      Complete
-                    </div>
-                  </td>
-
-                  <td className="whitespace-no-wrap hidden py-4 text-sm font-normal text-gray-500 sm:px-6 lg:table-cell">
-                    <div className="inline-flex items-center rounded-full bg-black py-2 px-3 text-xs text-white">
-                      Complete
-                    </div>
-                  </td>
-                </tr>
-
-                <tr>
-                  <td width="50%" className="whitespace-no-wrap py-4 text-sm font-bold text-gray-700 sm:px-6">
-                    #000002
-                    <div className="mt-1 lg:hidden">
-                      <p className="font-normal text-gray-500">14 November, 2021</p>
-                    </div>
-                  </td>
-
-                  <td className="whitespace-no-wrap hidden py-4 text-sm font-normal text-gray-500 sm:px-6 lg:table-cell">
-                    14 November, 2021
-                  </td>
-
-                  <td className="whitespace-no-wrap py-4 px-6 text-right text-sm text-gray-600 lg:text-left">
-                    500.00 THB
-                    <div className="flex mt-1 ml-auto w-fit items-center rounded-full bg-gray-500 py-1 px-2 text-left font-bold text-white lg:hidden">
-                      Pending
-                    </div>
-                  </td>
-
-                  <td className="whitespace-no-wrap hidden py-4 text-sm font-normal text-gray-500 sm:px-6 lg:table-cell">
-                    <div className="inline-flex items-center rounded-full bg-gray-500 py-1 px-2 text-white">
-                      Pending
-                    </div>
-                  </td>
-                </tr>
-
-                <tr>
-                  <td width="50%" className="whitespace-no-wrap py-4 text-sm font-bold text-gray-700 sm:px-6">
-                    #000003
-                    <div className="mt-1 lg:hidden">
-                      <p className="font-normal text-gray-500">15 October, 2021</p>
-                    </div>
-                  </td>
-
-                  <td className="whitespace-no-wrap hidden py-4 text-sm font-normal text-gray-500 sm:px-6 lg:table-cell">
-                    15 October, 2021
-                  </td>
-
-                  <td className="whitespace-no-wrap py-4 px-6 text-right text-sm text-gray-600 lg:text-left">
-                    500.00 THB
-                    <div className="flex mt-1 ml-auto w-fit items-center rounded-full bg-black py-2 px-3 text-left text-xs font-bold text-white lg:hidden">
-                      Complete
-                    </div>
-                  </td>
-
-                  <td className="whitespace-no-wrap hidden py-4 text-sm font-normal text-gray-500 sm:px-6 lg:table-cell">
-                    <div className="inline-flex items-center rounded-full bg-black py-2 px-3 text-xs text-white">
-                      Complete
-                    </div>
-                  </td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
-        </div>
+        )}
       </div>
     </div>
   )
