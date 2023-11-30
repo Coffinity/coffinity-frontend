e495ffcc3c1e49c7215eff17964ea301f90ff2a4
imnotprn <pepand.napoleon@hotmail.com>
2023-11-29 19:26:01 -0800

    fix:stylecreate

--- src/pages/CreateProduct.tsx
+++ src/pages/CreateProduct.tsx
@@ -55,46 +55,86 @@ export const CreateProduct = () => {
   }
 
   return (
-    <div className="">
-      <form className="h-screen flex justify-center items-center bg-cover" onSubmit={handleSubmit}>
-        <label>Product Name</label>
-        <input type="text" onChange={(e) => setNewProductName(e.target.value)} required />
-        <label>Product Description</label>
-        <input type="text" onChange={(e) => setNewProductDescription(e.target.value)} required />
-        <label>Image</label>
-        <input type="file" accept="image/" onChange={handleProductImageUpload} required />
+    <div className="min-h-screen flex justify-center items-center bg-allPageBg bg-cover ">
+      <div className="relative space-y-3 rounded-md bg-white bg-opacity-80 p-6 shadow-xl lg:p-10 border border-gray-100 mt-56 mb-36">
+        <form className="grid grid-cols-1" onSubmit={handleSubmit}>
+          <div className="grid grid-cols-1 mb-3">
+            <label>Product Name : </label>
+            <input
+              type="text"
+              className="mt-2 h-12 w-full rounded-md bg-gray-100 px-3 outline-none focus:ring"
+              onChange={(e) => setNewProductName(e.target.value)}
+              required
+            />
+          </div>
 
-        <select
-          className="select select-bordered w-full max-w-xs"
-          defaultValue={newTypes}
-          onChange={(e) => setnewTypes(e.target.value)}
-          required
-        >
-          <option value={newTypes} disabled selected>
-            Types
-          </option>
-          <option value={'Coffee Beans'}>Coffee Beans</option>
-          <option value={'Equipments'}>Equipments</option>
-        </select>
+          <div className="grid grid-cols-1 mb-3">
+            <label>Product Description : </label>
+            <input
+              className="mt-2 h-12 w-full rounded-md bg-gray-100 px-3 outline-none focus:ring"
+              type="text"
+              onChange={(e) => setNewProductDescription(e.target.value)}
+              required
+            />
+          </div>
 
-        <label>Price</label>
-        <input type="number" onChange={(e) => setNewPrice(e.target.valueAsNumber)} required />
-        <label>Quantity</label>
-        <input type="number" onChange={(e) => setNewStockQuantity(e.target.valueAsNumber)} required />
-        <div>
-          {newImage ? (
-            <div>
-              <img src={newImage as string} alt="product image" />
-            </div>
-          ) : (
-            <p>Image preview will appear here!</p>
-          )}
-        </div>
+          <div className="grid grid-cols-1 mb-4">
+            <label className="mb-2">Image</label>
+            <input type="file" accept="image/" onChange={handleProductImageUpload} required />
+          </div>
 
-        <button type="submit" className="bg-black">
-          Submit
-        </button>
-      </form>
+          <div className="grid grid-cols-1 mb-4">
+            <select
+              className="select select-bordered w-full max-w-xs"
+              defaultValue={newTypes}
+              onChange={(e) => setnewTypes(e.target.value)}
+              required
+            >
+              <option value={newTypes} disabled selected>
+                Types
+              </option>
+              <option value={'Coffee Beans'}>Coffee Beans</option>
+              <option value={'Equipments'}>Equipments</option>
+            </select>
+          </div>
+
+          <div className=" grid grid-cols-1 mb-4">
+            <label>Price : </label>
+            <input
+              className="mt-2 h-12 w-full rounded-md bg-gray-100 px-3 outline-none focus:ring"
+              type="number"
+              onChange={(e) => setNewPrice(e.target.valueAsNumber)}
+              required
+            />
+          </div>
+
+          <div className="grid grid-cols-1 mb-4">
+            <label>Quantity : </label>
+            <input
+              className="mt-2 h-12 w-full rounded-md bg-gray-100 px-3 outline-none focus:ring"
+              type="number"
+              onChange={(e) => setNewStockQuantity(e.target.valueAsNumber)}
+              required
+            />
+          </div>
+
+          <div className="flex my-5 justify-center w-full">
+            {newImage ? (
+              <img className="rounded-lg w-[150px]" src={newImage as string} alt="product image" />
+            ) : (
+              <p>Image preview will appear here!</p>
+            )}
+          </div>
+          <div className="flex justify-center">
+            <button
+              type="submit"
+              className=" mt-5 w-1/4 rounded-md bg-black p-2  text-center font-semibold text-white outline-none focus:ring"
+            >
+              Submit
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   )
 }
